a6611eb49f0c50ef72a75532d42f364771c40715
Ofek Basson <bas.ofek@gmail.com>
2023-03-11 09:03:28 +0200

    First commit

---
+++ ps0_python/ps0.py
@@ -0,0 +1,81 @@
+from io import BytesIO
+from PIL import Image
+import numpy as np
+import requests
+from fpdf import FPDF
+import os
+
+#Saving wide image
+first_response = requests.get('https://img.mako.co.il/2023/03/06/ANGRY_BIBI_LEVIN_1502_re_autoOrient_x6.jpg')
+wide_img = Image.open(BytesIO(first_response.content))
+new_wide_image_size = (512, 288)
+resized_wide_img = wide_img.resize(new_wide_image_size)
+resized_wide_img.save('output/ps0-1-a-1.png')
+
+#Saving tall image
+second_response = requests.get('https://images.pexels.com/photos/699466/pexels-photo-699466.jpeg?auto=compress&cs=tinysrgb&w=1600')
+tall_img = Image.open(BytesIO(second_response.content))
+new_tall_image_size = (384, 512)
+resized_tall_img = tall_img.resize(new_tall_image_size)
+resized_tall_img.save('output/ps0-1-a-2.png')
+
+#Swapping red and blue
+resized_wide_image_as_ndarray = np.asarray(resized_wide_img)
+swapped_blue_red_resized_wide_image_as_ndarray = resized_wide_image_as_ndarray[:,:,[2,1,0]]
+swapped_blue_red_resized_wide_image = Image.fromarray(swapped_blue_red_resized_wide_image_as_ndarray, 'RGB')
+swapped_blue_red_resized_wide_image.save('output/ps0-2-a-1.png')
+
+#Monochrome image - only green
+wide_image_as_ndarray = np.asarray(resized_wide_img)
+monochrome_wide_green_image_as_ndarray = wide_image_as_ndarray[:,:,1]
+monochrome_wide_image = Image.fromarray(monochrome_wide_green_image_as_ndarray, 'L')
+monochrome_wide_image.save('output/ps0-2-b-1.png')
+
+#Monochrome image - only red
+wide_image_as_ndarray = np.asarray(resized_wide_img)
+monochrome_wide_red_image_as_ndarray = wide_image_as_ndarray[:,:,0]
+monochrome_wide_image = Image.fromarray(monochrome_wide_red_image_as_ndarray, 'L')
+monochrome_wide_image.save('output/ps0-2-c-1.png')
+
+#Replacement of pixels
+shape_of_wide_image = monochrome_wide_green_image_as_ndarray.shape
+center_coordinates_of_wide_image = tuple(int(coordinate/2) for coordinate in shape_of_wide_image)
+center_square_coordinates_of_wide_image = list([coordinate - 50, coordinate + 50] for coordinate in center_coordinates_of_wide_image)
+
+lower_border = center_square_coordinates_of_wide_image[0][0]
+upper_border = center_square_coordinates_of_wide_image[0][1]
+left_border = center_square_coordinates_of_wide_image[1][0]
+right_border = center_square_coordinates_of_wide_image[1][1]
+
+center_square_region_of_red_monochrome_image = monochrome_wide_green_image_as_ndarray[lower_border:upper_border, left_border:right_border]
+
+new_monochrome_wide_red_image_as_ndarray = np.copy(monochrome_wide_red_image_as_ndarray)
+new_monochrome_wide_red_image_as_ndarray[lower_border:upper_border, left_border:right_border] = center_square_region_of_red_monochrome_image
+new_monochrome_wide_red_image = Image.fromarray(new_monochrome_wide_red_image_as_ndarray)
+new_monochrome_wide_red_image.save('output/ps0-3-a-1.png')
+
+
+#Answers in PDF
+pdf = FPDF()
+pdf.add_page()
+pdf.set_font('helvetica', '', 16)
+
+directory = 'output'
+for filename in reversed(os.listdir(directory)):
+    potential_file = os.path.join(directory, filename)
+    if os.path.isfile(potential_file):
+        pdf.cell(50, 10, filename, ln=True)
+        pdf.image(potential_file, w=100)
+
+pdf.output('ps0_report.pdf')
+
+
+
+
+
+
+
+
+
+
+
