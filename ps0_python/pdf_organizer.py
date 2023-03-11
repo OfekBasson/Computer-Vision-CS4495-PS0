cef62030ab85a11a221460120dac1a617ea0108e
Ofek Basson <bas.ofek@gmail.com>
2023-03-11 11:53:52 +0200

    Creating 'PDF_Organizer' class

---
+++ ps0_python/pdf_organizer.py
@@ -0,0 +1,20 @@
+from fpdf import FPDF
+import os
+from PIL import Image
+
+
+
+class PDF_Organizer(FPDF):
+    def __init__(self):
+        FPDF.__init__(self)
+
+    # Adding images to pdf
+    def save_and_add_title_and_image_to_pdf(self, img, filename, mode, section=None):
+        file_path = os.path.join('output', filename)
+        if type(img) is not Image.Image:
+            img = Image.fromarray(img, mode)
+        img.save(file_path)
+        if section is not None:
+            self.cell(10, 10, section, ln=True)
+        self.cell(50, 10, filename, ln=True)
+        self.image(file_path, w=120)
--- ps0_python/ps0.py
+++ ps0_python/ps0.py
@@ -2,51 +2,39 @@ from io import BytesIO
 from PIL import Image
 import numpy as np
 import requests
-from fpdf import FPDF
-import os
+from pdf_organizer import PDF_Organizer
 
 #Preparing PDF
-pdf = FPDF()
+pdf = PDF_Organizer()
 pdf.add_page()
 pdf.set_font('helvetica', '', 16)
-
-#Adding images to pdf
-def save_and_add_title_and_image_to_pdf(img, filename, mode, section=None):
-    file_path = os.path.join('output', filename)
-    if type(img) is not Image.Image:
-        img = Image.fromarray(img, mode)
-    img.save(file_path)
-    if section is not None:
-        pdf.cell(10, 10, section, ln=True)
-    pdf.cell(50, 10, filename, ln=True)
-    pdf.image(file_path, w=120)
 
 #Saving wide image
 first_response = requests.get('https://img.mako.co.il/2023/03/06/ANGRY_BIBI_LEVIN_1502_re_autoOrient_x6.jpg')
 wide_img = Image.open(BytesIO(first_response.content))
 new_wide_image_size = (512, 288)
 resized_wide_img = wide_img.resize(new_wide_image_size)
-save_and_add_title_and_image_to_pdf(resized_wide_img, 'ps0-1-a-1.png', 'RGB', section='1.a.')
+pdf.save_and_add_title_and_image_to_pdf(resized_wide_img, 'ps0-1-a-1.png', 'RGB', section='1.a.')
 
 #Saving tall image
 second_response = requests.get('https://images.pexels.com/photos/699466/pexels-photo-699466.jpeg?auto=compress&cs=tinysrgb&w=1600')
 tall_img = Image.open(BytesIO(second_response.content))
 new_tall_image_size = (384, 512)
 resized_tall_img = tall_img.resize(new_tall_image_size)
-save_and_add_title_and_image_to_pdf(resized_tall_img, 'ps0-1-a-2.png', 'RGB')
+pdf.save_and_add_title_and_image_to_pdf(resized_tall_img, 'ps0-1-a-2.png', 'RGB')
 
 #Swapping red and blue
 resized_wide_image_as_ndarray = np.asarray(resized_wide_img)
 swapped_blue_red_resized_wide_image_as_ndarray = resized_wide_image_as_ndarray[:, :, [2, 1, 0]]
-save_and_add_title_and_image_to_pdf(swapped_blue_red_resized_wide_image_as_ndarray, 'ps0-2-a-1.png', 'RGB', section='2.a.')
+pdf.save_and_add_title_and_image_to_pdf(swapped_blue_red_resized_wide_image_as_ndarray, 'ps0-2-a-1.png', 'RGB', section='2.a.')
 
 #Monochrome image - only green
 monochrome_wide_green_image_as_ndarray = resized_wide_image_as_ndarray[:, :, 1]
-save_and_add_title_and_image_to_pdf(monochrome_wide_green_image_as_ndarray, 'ps0-2-b-1.png', 'L', '2.b.')
+pdf.save_and_add_title_and_image_to_pdf(monochrome_wide_green_image_as_ndarray, 'ps0-2-b-1.png', 'L', '2.b.')
 
 #Monochrome image - only red
 monochrome_wide_red_image_as_ndarray = resized_wide_image_as_ndarray[:, :, 0]
-save_and_add_title_and_image_to_pdf(monochrome_wide_red_image_as_ndarray, 'ps0-2-c-1.png', 'L', '2.c.')
+pdf.save_and_add_title_and_image_to_pdf(monochrome_wide_red_image_as_ndarray, 'ps0-2-c-1.png', 'L', '2.c.')
 
 #Replacement of pixels
 shape_of_wide_image = monochrome_wide_green_image_as_ndarray.shape
@@ -63,8 +51,7 @@ center_square_region_of_red_monochrome_image = monochrome_wide_green_image_as_nd
 new_monochrome_wide_red_image_as_ndarray = np.copy(monochrome_wide_red_image_as_ndarray)
 new_monochrome_wide_red_image_as_ndarray[lower_border:upper_border, left_border:right_border] = center_square_region_of_red_monochrome_image
 
-save_and_add_title_and_image_to_pdf(new_monochrome_wide_red_image_as_ndarray, 'ps0-3-a-1.png', 'L', '3.a.')
-
+pdf.save_and_add_title_and_image_to_pdf(new_monochrome_wide_red_image_as_ndarray, 'ps0-3-a-1.png', 'L', '3.a.')
 
 #Arithmetic and geometric operations:
 pdf.cell(10, 10, '4.a.')
@@ -80,16 +67,16 @@ normalized_wide_green_image_as_ndarray -= int(mean_pixel_value_of_img1_green)
 normalized_wide_green_image_as_ndarray = normalized_wide_green_image_as_ndarray / std_value_of_img1_green
 normalized_wide_green_image_as_ndarray *= 10
 normalized_wide_green_image_as_ndarray += int(mean_pixel_value_of_img1_green)
-save_and_add_title_and_image_to_pdf(normalized_wide_green_image_as_ndarray, 'ps0-4-b-1.png', 'L', '4.b.')
+pdf.save_and_add_title_and_image_to_pdf(normalized_wide_green_image_as_ndarray, 'ps0-4-b-1.png', 'L', '4.b.')
 
 #Shifted_green_image_as_ndarray = np.copy(monochrome_wide_green_image_as_ndarray)
 shifted_green_image_as_ndarray = np.zeros((monochrome_wide_green_image_as_ndarray.shape[0], monochrome_wide_green_image_as_ndarray.shape[1]), dtype=np.uint8)
 shifted_green_image_as_ndarray[:, :-2] = monochrome_wide_green_image_as_ndarray[:, 2:]
-save_and_add_title_and_image_to_pdf(shifted_green_image_as_ndarray, 'ps0-4-c-1.png', 'L', '4.c.')
+pdf.save_and_add_title_and_image_to_pdf(shifted_green_image_as_ndarray, 'ps0-4-c-1.png', 'L', '4.c.')
 
 #Substract shifted version of green img from the original
 substracted_image_as_ndarray = monochrome_wide_green_image_as_ndarray - shifted_green_image_as_ndarray
-save_and_add_title_and_image_to_pdf(substracted_image_as_ndarray, 'ps0-4-d-1.png', 'L', '4.d.')
+pdf.save_and_add_title_and_image_to_pdf(substracted_image_as_ndarray, 'ps0-4-d-1.png', 'L', '4.d.')
 pdf.cell(100, 10, 'Negative values become 0.', ln=True)
 
 pdf.output('ps0_report.pdf')
