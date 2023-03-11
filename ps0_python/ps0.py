16c21c05c14c040cdfaa0c2bcfe714d41f3986ba
Ofek Basson <bas.ofek@gmail.com>
2023-03-11 11:39:15 +0200

    Ordering the code and creating a function of saving and adding pictures to pdf

--- ps0_python/ps0.py
+++ ps0_python/ps0.py
@@ -10,37 +10,43 @@ pdf = FPDF()
 pdf.add_page()
 pdf.set_font('helvetica', '', 16)
 
+#Adding images to pdf
+def save_and_add_title_and_image_to_pdf(img, filename, mode, section=None):
+    file_path = os.path.join('output', filename)
+    if type(img) is not Image.Image:
+        img = Image.fromarray(img, mode)
+    img.save(file_path)
+    if section is not None:
+        pdf.cell(10, 10, section, ln=True)
+    pdf.cell(50, 10, filename, ln=True)
+    pdf.image(file_path, w=120)
+
 #Saving wide image
 first_response = requests.get('https://img.mako.co.il/2023/03/06/ANGRY_BIBI_LEVIN_1502_re_autoOrient_x6.jpg')
 wide_img = Image.open(BytesIO(first_response.content))
 new_wide_image_size = (512, 288)
 resized_wide_img = wide_img.resize(new_wide_image_size)
-resized_wide_img.save('output/ps0-1-a-1.png')
+save_and_add_title_and_image_to_pdf(resized_wide_img, 'ps0-1-a-1.png', 'RGB', section='1.a.')
 
 #Saving tall image
 second_response = requests.get('https://images.pexels.com/photos/699466/pexels-photo-699466.jpeg?auto=compress&cs=tinysrgb&w=1600')
 tall_img = Image.open(BytesIO(second_response.content))
 new_tall_image_size = (384, 512)
 resized_tall_img = tall_img.resize(new_tall_image_size)
-resized_tall_img.save('output/ps0-1-a-2.png')
+save_and_add_title_and_image_to_pdf(resized_tall_img, 'ps0-1-a-2.png', 'RGB')
 
 #Swapping red and blue
 resized_wide_image_as_ndarray = np.asarray(resized_wide_img)
-swapped_blue_red_resized_wide_image_as_ndarray = resized_wide_image_as_ndarray[:,:,[2,1,0]]
-swapped_blue_red_resized_wide_image = Image.fromarray(swapped_blue_red_resized_wide_image_as_ndarray, 'RGB')
-swapped_blue_red_resized_wide_image.save('output/ps0-2-a-1.png')
+swapped_blue_red_resized_wide_image_as_ndarray = resized_wide_image_as_ndarray[:, :, [2, 1, 0]]
+save_and_add_title_and_image_to_pdf(swapped_blue_red_resized_wide_image_as_ndarray, 'ps0-2-a-1.png', 'RGB', section='2.a.')
 
 #Monochrome image - only green
-wide_image_as_ndarray = np.asarray(resized_wide_img)
-monochrome_wide_green_image_as_ndarray = wide_image_as_ndarray[:,:,1]
-monochrome_wide_image = Image.fromarray(monochrome_wide_green_image_as_ndarray, 'L')
-monochrome_wide_image.save('output/ps0-2-b-1.png')
+monochrome_wide_green_image_as_ndarray = resized_wide_image_as_ndarray[:, :, 1]
+save_and_add_title_and_image_to_pdf(monochrome_wide_green_image_as_ndarray, 'ps0-2-b-1.png', 'L', '2.b.')
 
 #Monochrome image - only red
-wide_image_as_ndarray = np.asarray(resized_wide_img)
-monochrome_wide_red_image_as_ndarray = wide_image_as_ndarray[:,:,0]
-monochrome_wide_image = Image.fromarray(monochrome_wide_red_image_as_ndarray, 'L')
-monochrome_wide_image.save('output/ps0-2-c-1.png')
+monochrome_wide_red_image_as_ndarray = resized_wide_image_as_ndarray[:, :, 0]
+save_and_add_title_and_image_to_pdf(monochrome_wide_red_image_as_ndarray, 'ps0-2-c-1.png', 'L', '2.c.')
 
 #Replacement of pixels
 shape_of_wide_image = monochrome_wide_green_image_as_ndarray.shape
@@ -56,13 +62,14 @@ center_square_region_of_red_monochrome_image = monochrome_wide_green_image_as_nd
 
 new_monochrome_wide_red_image_as_ndarray = np.copy(monochrome_wide_red_image_as_ndarray)
 new_monochrome_wide_red_image_as_ndarray[lower_border:upper_border, left_border:right_border] = center_square_region_of_red_monochrome_image
-new_monochrome_wide_red_image = Image.fromarray(new_monochrome_wide_red_image_as_ndarray, 'L')
-new_monochrome_wide_red_image.save('output/ps0-3-a-1.png')
+
+save_and_add_title_and_image_to_pdf(new_monochrome_wide_red_image_as_ndarray, 'ps0-3-a-1.png', 'L', '3.a.')
 
 
 #Arithmetic and geometric operations:
+pdf.cell(10, 10, '4.a.')
 max_pixel_value_of_img1_green = np.amax(monochrome_wide_green_image_as_ndarray)
-pdf.cell(100, 10, f'4.a. Max pixel value of img1 green is: {max_pixel_value_of_img1_green}', ln=True)
+pdf.cell(100, 10, f'Max pixel value of img1 green is: {max_pixel_value_of_img1_green}', ln=True)
 mean_pixel_value_of_img1_green = np.mean(monochrome_wide_green_image_as_ndarray)
 pdf.cell(100, 10, f'Mean pixel value of img1 green is: {mean_pixel_value_of_img1_green}', ln=True)
 std_value_of_img1_green = np.std(monochrome_wide_green_image_as_ndarray)
@@ -73,28 +80,17 @@ normalized_wide_green_image_as_ndarray -= int(mean_pixel_value_of_img1_green)
 normalized_wide_green_image_as_ndarray = normalized_wide_green_image_as_ndarray / std_value_of_img1_green
 normalized_wide_green_image_as_ndarray *= 10
 normalized_wide_green_image_as_ndarray += int(mean_pixel_value_of_img1_green)
-normalized_wide_green_image = Image.fromarray(normalized_wide_green_image_as_ndarray, 'L')
-normalized_wide_green_image.save('output/ps0-4-b-1.png')
+save_and_add_title_and_image_to_pdf(normalized_wide_green_image_as_ndarray, 'ps0-4-b-1.png', 'L', '4.b.')
 
 #Shifted_green_image_as_ndarray = np.copy(monochrome_wide_green_image_as_ndarray)
 shifted_green_image_as_ndarray = np.zeros((monochrome_wide_green_image_as_ndarray.shape[0], monochrome_wide_green_image_as_ndarray.shape[1]), dtype=np.uint8)
 shifted_green_image_as_ndarray[:, :-2] = monochrome_wide_green_image_as_ndarray[:, 2:]
-shifted_green_image = Image.fromarray(shifted_green_image_as_ndarray)
-shifted_green_image.save('output/ps0-4-c-1.png')
+save_and_add_title_and_image_to_pdf(shifted_green_image_as_ndarray, 'ps0-4-c-1.png', 'L', '4.c.')
 
 #Substract shifted version of green img from the original
 substracted_image_as_ndarray = monochrome_wide_green_image_as_ndarray - shifted_green_image_as_ndarray
-substracted_image = Image.fromarray(substracted_image_as_ndarray)
-substracted_image.save('output/ps0-4-d-1.png')
-pdf.cell(100, 10, '4.d. Negative values become 0.', ln=True)
-
-#Adding images to pdf
-directory = 'output'
-for filename in reversed(os.listdir(directory)):
-    potential_file = os.path.join(directory, filename)
-    if os.path.isfile(potential_file):
-        pdf.cell(50, 10, filename, ln=True)
-        pdf.image(potential_file, w=105)
+save_and_add_title_and_image_to_pdf(substracted_image_as_ndarray, 'ps0-4-d-1.png', 'L', '4.d.')
+pdf.cell(100, 10, 'Negative values become 0.', ln=True)
 
 pdf.output('ps0_report.pdf')
 
