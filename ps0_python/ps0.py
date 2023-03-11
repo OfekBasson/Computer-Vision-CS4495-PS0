2f5d6ea11751b6d58b6af03f4705f60818955b84
Ofek Basson <bas.ofek@gmail.com>
2023-03-11 09:35:00 +0200

    Some changes in the order of the code and start od section 4

--- ps0_python/ps0.py
+++ ps0_python/ps0.py
@@ -5,6 +5,11 @@ import requests
 from fpdf import FPDF
 import os
 
+#Preparing PDF
+pdf = FPDF()
+pdf.add_page()
+pdf.set_font('helvetica', '', 16)
+
 #Saving wide image
 first_response = requests.get('https://img.mako.co.il/2023/03/06/ANGRY_BIBI_LEVIN_1502_re_autoOrient_x6.jpg')
 wide_img = Image.open(BytesIO(first_response.content))
@@ -55,17 +60,31 @@ new_monochrome_wide_red_image = Image.fromarray(new_monochrome_wide_red_image_as
 new_monochrome_wide_red_image.save('output/ps0-3-a-1.png')
 
 
-#Answers in PDF
-pdf = FPDF()
-pdf.add_page()
-pdf.set_font('helvetica', '', 16)
+#Arithmetic and geometric operations:
+max_pixel_value_of_img1_green = np.amax(monochrome_wide_green_image_as_ndarray)
+pdf.cell(100, 10, f'Max pixel value of img1 green is: {max_pixel_value_of_img1_green}', ln=True)
+mean_pixel_value_of_img1_green = np.mean(monochrome_wide_green_image_as_ndarray)
+pdf.cell(100, 10, f'Mean pixel value of img1 green is: {mean_pixel_value_of_img1_green}', ln=True)
+std_value_of_img1_green = np.std(monochrome_wide_green_image_as_ndarray)
+pdf.cell(100, 10, f'Std value of img1 green is: {std_value_of_img1_green}', ln=True)
 
+normalized_wide_green_image_as_ndarray = np.copy(monochrome_wide_green_image_as_ndarray)
+normalized_wide_green_image_as_ndarray -= int(mean_pixel_value_of_img1_green)
+normalized_wide_green_image_as_ndarray = normalized_wide_green_image_as_ndarray / std_value_of_img1_green
+normalized_wide_green_image_as_ndarray *= 10
+normalized_wide_green_image_as_ndarray += int(mean_pixel_value_of_img1_green)
+normalized_wide_green_image = Image.fromarray(normalized_wide_green_image_as_ndarray, 'L')
+normalized_wide_green_image.save('output/ps0-4-b-1.png')
+
+
+
+#Adding images to pdf
 directory = 'output'
 for filename in reversed(os.listdir(directory)):
     potential_file = os.path.join(directory, filename)
     if os.path.isfile(potential_file):
         pdf.cell(50, 10, filename, ln=True)
-        pdf.image(potential_file, w=100)
+        pdf.image(potential_file, w=105)
 
 pdf.output('ps0_report.pdf')
 
